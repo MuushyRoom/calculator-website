35e94472d4abc91f587c2dace277b93ae1523bb5
MuushyRoom <ronancuaresma10@gmail.com>
2025-02-13 22:23:41 +0800

    functionality close to 60% will fix some bugs and user error management

--- indexScripts.js
+++ indexScripts.js
@@ -1,11 +1,3 @@
-let num1,num2;
-function setClear(){
-    num1 = 0;
-    num2 = 0;
 
-}
-function showNewNum(){
-    document.getElementById("displayArea").scrollLeft = document.getElementById("displayArea").scrollWidth
- 
-}
+
 
